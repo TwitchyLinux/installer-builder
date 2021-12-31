f328d3c9d10e7c5a6c96b721093838c26790f3f4
Tom <twitchyliquid64@ciphersink.net>
2021-12-30 18:53:41 -0800

    Fix issue with chrome not working

--- configuration/overlay_twlinst.nix
+++ configuration/overlay_twlinst.nix
@@ -3,13 +3,13 @@
 
   super.buildGoModule rec {
    name = "twlinst";
-   version = "63f9a3d17ae6afd7adc415dbfd3c5c2f3a81b2e3";
+   version = "04c922b825a7558b7416d6b9ff2234e70cad64a9";
 
    src = super.fetchFromGitHub {
      owner = "twitchylinux";
      repo = "twlinst";
      rev = "${version}";
-     sha256 = "0y1my8j9iswfmg0pzwyfpq32g03akbsrjql4rdn1cf4mcwkpgmpd"; # use nix-prefetch-git
+     sha256 = "1ig3hiyh5sj4ijh9dficfggdiw7fbf72jbysn09ra333prm32sya"; # use nix-prefetch-git
    };
 
    vendorSha256 = "sha256-FKB3YiM/zkkW5olfnaCw4AYI7YvcpvLyLSP6xHMd5mY=";
