2a4c19322c883f67f4c7ffb7f82f0ab816934fd2
Tom <twitchyliquid64@ciphersink.net>
2023-08-31 16:53:14 -0700

    Update for 23.05

--- sources.nix
+++ sources.nix
@@ -5,12 +5,12 @@
   # This is imported into the nixos configuration for both the installer
   # and the final TwitchyLinux installation.
   twl-base-config = builtins.fetchTarball {
-     url = "https://github.com/TwitchyLinux/base-config/archive/refs/tags/v0.1.15.tar.gz";
-     sha256 = "0jsml5mixn0clri5asy1i4k5hli49rsxri2zdylgq2qh29svms0b"; # use nix-prefetch-url --unpack '<url>'
+     url = "https://github.com/TwitchyLinux/base-config/archive/refs/tags/v0.1.16.tar.gz";
+     sha256 = "0vpj9dh93zvlp73ksnm78c9p485iqblq5ry3lg4cnwv7cghanivg"; # use nix-prefetch-url --unpack '<url>'
   };
 
   nixos-hardware = builtins.fetchTarball {
-    url = "https://github.com/NixOS/nixos-hardware/archive/3006d2860a6ed5e01b0c3e7ffb730e9b293116e2.tar.gz";
-    sha256 = "1xds9sy8hw98ss7fv5d2cqa1nf3agq4bm0x3appbawrhzp275zvq"; # use nix-prefetch-url --unpack '<url>'
+    url = "https://github.com/NixOS/nixos-hardware/archive/817e297fc3352fadc15f2c5306909aa9192d7d97.tar.gz";
+    sha256 = "1mm2kdbrgqvnah813znigxyzmrhzi6ccdndbs68m4hzyzlzfvjjf"; # use nix-prefetch-url --unpack '<url>'
   };
 }
